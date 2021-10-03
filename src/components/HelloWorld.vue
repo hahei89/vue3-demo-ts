3051b179797f7c1f8492e918da21f45323a2a77d
litianhua <18702971504@163.com>
2021-10-03 09:37:47 +0800

    增加ts配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,23 @@
-<script setup>
-import { ref } from 'vue'
-
+<script setup lang="ts">
+import css from '../assets/index.css'
+import { ref, onMounted } from 'vue'
+const color = "red"
+const font = {
+  size: '12px'
+}
 defineProps({
   msg: String
 })
 
 const count = ref(0)
+onMounted: {
+  console.log('on Mounted')
+}
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+  <h1 class="text">{{ msg }}</h1>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
@@ -34,7 +27,14 @@ const count = ref(0)
 </template>
 
 <style scoped>
+:root {
+  --varColor: #42b983;
+}
 a {
-  color: #42b983;
+  color: var(--varColor);
+}
+.text {
+  color: v-bind(color);
+  font-size: v-bind("font.size");
 }
 </style>
