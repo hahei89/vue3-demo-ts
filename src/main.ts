6e36b507cd2f8f87512821566db552a9255974d7
litianhua <18702971504@163.com>
2021-10-04 22:37:02 +0800

    引入ant-desing-vue@next

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import 'ant-design-vue/dist/antd.css';
+import Antd from 'ant-design-vue';
 
-createApp(App).mount('#app')
+import App from './App.vue'
+const app = createApp(App)
+app.config.productionTip = false
+app.use(Antd).mount('#app')
