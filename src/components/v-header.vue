18da0ded80ae51163cb3ebdb5528e5ed28974a9b
litianhua <18702971504@163.com>
2021-10-06 21:52:33 +0800

    引入tailwindcss

---
+++ src/components/v-header.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-row>
+    <a-col :span="8">
+      践知
+    </a-col>
+    <a-col :span="8" :offset="8">
+      个人信息
+    </a-col>
+  </a-row>
+</template>
+<script setup lang="ts">
+
+</script>
+<style lang="less">
+  
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import store, { key } from './store/index.ts'
 import router from './router/index.ts'
 import '@mestoness/resetcss/dist/css/resetCss.min.css'
 
-// 自定义主，，覆盖antd默认主题
-import '@/assets/antd-reset.less'
 import Antd from 'ant-design-vue'
+// 自定义主题，覆盖antd默认主题
+import '@/assets/antd-reset.less'
+import '@/assets/index.css'
 
 import App from './App.vue'
 const app = createApp(App)
