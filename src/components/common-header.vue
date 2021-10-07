bb32fe22d0fe43e4d31bb07ebe744ba070d6fd3e
litianhua <18702971504@163.com>
2021-10-07 22:25:42 +0800

    使用antd图标

--- src/components/common-header.vue
+++ src/components/common-header.vue
@@ -1,16 +1,70 @@
 <template>
-  <a-row>
-    <a-col :span="8">
-      践知
+  <a-row class="header">
+    <a-col :span="8" class="header-left">
+      <a-row>
+        <img class="home-img" src="../assets/imgs/home.jpeg" alt="首页" srcset="">
+        <span class="home-text">践知</span>
+      </a-row>
     </a-col>
-    <a-col :span="8" :offset="8">
-      个人信息
+    <a-col
+      :span="2"
+      :offset="14"
+      class="header-right">
+      <a-dropdown
+        :placement="placement"
+        :getPopupContainer="
+          triggerNode => {            
+            return triggerNode.parentNode
+          }">
+        <div>
+          <img class="avatar-img" src="../assets/imgs/home.jpeg" alt="头像" srcset="">
+          <DownOutlined />
+        </div>
+
+        <template #overlay>
+          <a-menu>
+            <a-menu-item>
+              <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
+            </a-menu-item>
+            <a-menu-item>
+              <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
+            </a-menu-item>
+            <a-menu-item>
+              <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
+            </a-menu-item>
+          </a-menu>
+        </template>
+      </a-dropdown>
     </a-col>
   </a-row>
 </template>
 <script setup lang="ts">
-
+  import { DownOutlined } from "@ant-design/icons-vue";
+  const placement = "bottomCenter";
 </script>
 <style lang="less">
-  
+  .avatar-box {
+    height: 32px;
+    border-radius: 50%;
+  }
+  .header {
+    .header-left {
+      .ant-row {
+        align-items: center;
+        font-size: 16px;
+        .home-img {
+          .avatar-box();
+        }
+        .home-text {
+          font-size: 16px;
+          margin-left: 4px;
+        }
+      }
+    }
+    .header-right {
+      .avatar-img {
+        .avatar-box();
+      }
+    }
+  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
-import store, { key } from './store/index.ts'
-import router from './router/index.ts'
+import App from './App.vue'
+import store, { key } from './store/index'
+import router from './router/index'
 import '@mestoness/resetcss/dist/css/resetCss.min.css'
 
 import Antd from 'ant-design-vue'
 // 自定义主题，覆盖antd默认主题
 import '@/assets/antd-reset.less'
-import '@/assets/index.css'
+// import '@/assets/index.css'
 
-import App from './App.vue'
 const app = createApp(App)
 app.config.productionTip = false
 app.use(Antd).use(router).use(store, key).mount('#app')
