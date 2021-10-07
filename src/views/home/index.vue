dac2a6912426f145c4d15553eaeb56fb8f926145
litianhua <18702971504@163.com>
2021-10-07 19:42:58 +0800

    test

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <a-layout-header>Header</a-layout-header>
+  <a-layout-header  class="border-b-2 border-gray-200 border-solid shadow-sm ring-2">
+    <v-header />
+  </a-layout-header>
   <a-layout>
-    <a-layout-sider>left sider</a-layout-sider>
+    <a-layout-sider>left l sider</a-layout-sider>
     <a-layout-content>Content</a-layout-content>
     <a-layout-sider>right Sider</a-layout-sider>
   </a-layout>
 </template>
 
 <script setup lang="ts">
-
+import VHeader from '@/components/v-header.vue'
 </script>
 
 <style>
