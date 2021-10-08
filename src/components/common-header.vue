8c6b7809472e2c772c1f50de93fe183f77c2378b
litianhua <18702971504@163.com>
2021-10-08 15:17:51 +0800

    修改格式

--- src/components/common-header.vue
+++ src/components/common-header.vue
@@ -2,35 +2,45 @@
   <a-row class="header">
     <a-col :span="8" class="header-left">
       <a-row>
-        <img class="home-img" src="../assets/imgs/home.jpeg" alt="首页" srcset="">
+        <img class="home-img" src="../assets/imgs/home.jpeg" alt="首页" />
         <span class="home-text">践知</span>
       </a-row>
     </a-col>
-    <a-col
-      :span="2"
-      :offset="14"
-      class="header-right">
+    <a-col :span="2" :offset="14" class="header-right">
       <a-dropdown
         :placement="placement"
         :getPopupContainer="
-          triggerNode => {            
-            return triggerNode.parentNode
-          }">
+        (triggerNode: HTMLElement) => {
+          return triggerNode.parentNode
+        }"
+      >
         <div>
-          <img class="avatar-img" src="../assets/imgs/home.jpeg" alt="头像" srcset="">
+          <img class="avatar-img" src="../assets/imgs/home.jpeg" alt="头像" />
           <DownOutlined />
         </div>
 
         <template #overlay>
           <a-menu>
             <a-menu-item>
-              <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://www.alipay.com/"
+              >1st menu item</a>
             </a-menu-item>
             <a-menu-item>
-              <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://www.taobao.com/"
+              >2nd menu item</a>
             </a-menu-item>
             <a-menu-item>
-              <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://www.tmall.com/"
+              >3rd menu item</a>
             </a-menu-item>
           </a-menu>
         </template>
@@ -39,32 +49,32 @@
   </a-row>
 </template>
 <script setup lang="ts">
-  import { DownOutlined } from "@ant-design/icons-vue";
-  const placement = "bottomCenter";
+import { DownOutlined } from "@ant-design/icons-vue";
+const placement = "bottomCenter";
 </script>
 <style lang="less">
-  .avatar-box {
-    height: 32px;
-    border-radius: 50%;
-  }
-  .header {
-    .header-left {
-      .ant-row {
-        align-items: center;
-        font-size: 16px;
-        .home-img {
-          .avatar-box();
-        }
-        .home-text {
-          font-size: 16px;
-          margin-left: 4px;
-        }
-      }
-    }
-    .header-right {
-      .avatar-img {
+.avatar-box {
+  height: 32px;
+  border-radius: 50%;
+}
+.header {
+  .header-left {
+    .ant-row {
+      align-items: center;
+      font-size: 16px;
+      .home-img {
         .avatar-box();
       }
+      .home-text {
+        font-size: 16px;
+        margin-left: 4px;
+      }
     }
   }
+  .header-right {
+    .avatar-img {
+      .avatar-box();
+    }
+  }
+}
 </style>
