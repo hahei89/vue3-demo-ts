47a31b61dccb21ba41f495164c8c707c505df52b
litianhua <18702971504@163.com>
2021-10-08 15:29:30 +0800

    去掉注释，使用tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import '@mestoness/resetcss/dist/css/resetCss.min.css'
 import Antd from 'ant-design-vue'
 // 自定义主题，覆盖antd默认主题
 import '@/assets/antd-reset.less'
-// import '@/assets/index.css'
+import '@/assets/index.css'
 
 const app = createApp(App)
 app.use(Antd).use(router).use(store, key).mount('#app')
