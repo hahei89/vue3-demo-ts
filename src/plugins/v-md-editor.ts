b4d72cb25b4ebc2be12bd0b1dae953aa9ee98705
litianhua <18702971504@163.com>
2021-10-09 10:29:37 +0800

    使用codemirror丰富Markdown功能

--- src/plugins/v-md-editor.ts
+++ src/plugins/v-md-editor.ts
@@ -1,8 +1,8 @@
 // plugin for v-md-editor
 import { App } from 'vue'
 // 富文本编辑器
-import VMdEditor from '@kangc/v-md-editor'
-import '@kangc/v-md-editor/lib/style/base-editor.css'
+import VMdEditor from '@kangc/v-md-editor/lib/codemirror-editor'
+import '@kangc/v-md-editor/lib/style/codemirror-editor.css';
 import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
 import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
 // Prism
@@ -10,7 +10,30 @@ import Prism from 'prismjs'
 // highlight code
 import 'prismjs/components/prism-json'
 
+// codemirror相关的资源
+import Codemirror from 'codemirror'
+// mode
+import 'codemirror/mode/markdown/markdown'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+import 'codemirror/mode/vue/vue'
+// edit
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/matchbrackets'
+// placeholder
+import 'codemirror/addon/display/placeholder'
+// active-line
+import 'codemirror/addon/selection/active-line'
+// scrollbar
+import 'codemirror/addon/scroll/simplescrollbars'
+import 'codemirror/addon/scroll/simplescrollbars.css'
+// style
+import 'codemirror/lib/codemirror.css'
+
 export default function (app: App) {
+  VMdEditor.Codemirror = Codemirror
   VMdEditor.use(vuepressTheme, {
     Prism
   })
